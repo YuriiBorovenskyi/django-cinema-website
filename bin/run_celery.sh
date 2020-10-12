9fd2a799abbad643f3458060903484b3888fe4c5
YuriiBorovenskyi <yurii.borovenskyi@gmail.com>
2020-10-12 16:10:29 +0300

    Deleted from .gitignore wildcard for all static and media files. Added to bash script 'run_celery.sh' python code, that will run checking of availability celery logging file and if necessary to create it.

--- bin/run_celery.sh
+++ bin/run_celery.sh
@@ -3,6 +3,8 @@
 # wait for Redis and DB server to start
 #sleep 10
 #echo "HELLO"
+
+python --command="from os.path import exists; log_file = 'logs/celery.log'; if not exists(log_file): with open(log_file, 'w') as f: pass"
 celery worker --app=django_cinema --loglevel=info --logfile=logs/celery.log
 
 exec "$@"
